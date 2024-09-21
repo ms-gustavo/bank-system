fcf7bd1c89c6e2ee74e27ebe63ff847b3b1e5253
Gustavo Maia <ms.gustavo@outlook.com>
2024-09-21 14:48:43 -0300

    fix

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,9 +1,10 @@
 import nodemailer from "nodemailer";
 import { SendEmailLoginProps } from "../types/interface";
+import dotenv from "dotenv";
+dotenv.config();
 
 const emailUser = process.env.EMAIL_USER!;
 const emailPass = process.env.EMAIL_PASS!;
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
