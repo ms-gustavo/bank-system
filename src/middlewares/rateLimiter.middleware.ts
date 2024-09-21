9c342c1f76b45e4891f758122bd6b469e6540ead
Gustavo Maia <ms.gustavo@outlook.com>
2024-09-21 13:27:12 -0300

    limite de reqs para login

---
+++ src/middlewares/rateLimiter.middleware.ts
@@ -0,0 +1,12 @@
+import rateLimit from "express-rate-limit";
+
+export class RateLimiter {
+  static loginRateLimiter() {
+    return rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 5,
+      message: `Muitas tentativas de login, tente novamente em 15 minutos`,
+      headers: true,
+    });
+  }
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,10 +3,16 @@ import { AuthController } from "../controllers/authController";
 import { validateDTO } from "../middlewares/validate.middleware";
 import { RegisterUserDTO } from "../dtos/registerUserDTO";
 import { LoginUserDTO } from "../dtos/loginUserDTO";
+import { RateLimiter } from "../middlewares/rateLimiter.middleware";
 
 const router = Router();
 
 router.post("/register", validateDTO(RegisterUserDTO), AuthController.register);
-router.post("/login", validateDTO(LoginUserDTO), AuthController.login);
+router.post(
+  "/login",
+  validateDTO(LoginUserDTO),
+  RateLimiter.loginRateLimiter(),
+  AuthController.login
+);
 
 export default router;
