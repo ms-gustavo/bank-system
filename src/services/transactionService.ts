db3aeb72ddc039fb8b53d8267f740489b84e5995
Gustavo Maia <ms.gustavo@outlook.com>
2024-09-22 00:37:57 -0300

    logs de transacao

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -4,49 +4,73 @@ import { CustomError } from "../utils/CustomError";
 
 class TransactionService {
   public async transfer({ clientId, merchantId, amount }: TransferProps) {
-    const client = await prisma.user.findUnique({
-      where: { id: clientId },
-    });
-
-    if (!client || client.role !== "CLIENT") {
-      throw new CustomError("Cliente não encontrado", 404);
-    }
-
-    if (client.balance < amount) {
-      throw new CustomError("Saldo insuficiente", 400);
-    }
-
-    const merchant = await prisma.user.findUnique({
-      where: { id: merchantId },
-    });
-
-    if (!merchant || merchant.role !== "MERCHANT") {
-      throw new CustomError("Estabelecimento não encontrado", 404);
-    }
-
-    const transaction = await prisma.$transaction(async (tx) => {
-      const updatedClient = await tx.user.update({
+    try {
+      const client = await prisma.user.findUnique({
         where: { id: clientId },
-        data: { balance: { decrement: amount } },
       });
 
-      const updatedMerchant = await tx.user.update({
+      if (!client || client.role !== "CLIENT") {
+        throw new CustomError("Cliente não encontrado", 404);
+      }
+
+      if (client.balance < amount) {
+        throw new CustomError("Saldo insuficiente", 400);
+      }
+
+      const merchant = await prisma.user.findUnique({
         where: { id: merchantId },
-        data: { balance: { increment: amount } },
       });
 
-      const newTransaction = await tx.transaction.create({
+      if (!merchant || merchant.role !== "MERCHANT") {
+        throw new CustomError("Estabelecimento não encontrado", 404);
+      }
+
+      const transaction = await prisma.$transaction(async (tx) => {
+        const updatedClient = await tx.user.update({
+          where: { id: clientId },
+          data: { balance: { decrement: amount } },
+        });
+
+        const updatedMerchant = await tx.user.update({
+          where: { id: merchantId },
+          data: { balance: { increment: amount } },
+        });
+
+        const newTransaction = await tx.transaction.create({
+          data: {
+            fromUserId: clientId,
+            toUserId: merchantId,
+            amount,
+          },
+        });
+
+        const transactionLog = await tx.transactionLog.create({
+          data: {
+            transactionId: newTransaction.id,
+            fromUserId: updatedClient.id,
+            toUserId: updatedMerchant.id,
+            amount,
+            status: "success",
+          },
+        });
+
+        return newTransaction;
+      });
+
+      return transaction;
+    } catch (error: unknown) {
+      await prisma.transactionLog.create({
         data: {
+          transactionId: "N/A",
           fromUserId: clientId,
           toUserId: merchantId,
           amount,
+          status: "failure",
+          errorMessage: (error as Error).message,
         },
       });
-
-      return newTransaction;
-    });
-
-    return transaction;
+      throw error;
+    }
   }
 }
 
